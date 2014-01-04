2ceadc33fc616f97a911cee998b09c7ec2d8ba2c
cynosureofhyd <tomu646@gmail.com>
2014-01-04 15:25:26 -0700

    First Addition of Coding Algorithms in Visual Studio 2012

---
+++ CodingInterviewProblems/RandomArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewProblems
+{
+    public class RandomArray
+    {
+        public static List<int> CreateRandomArray(int numberofelements)
+        {
+            Random rand = new Random();
+            
+            List<int> result = new List<int>();
+            for (int i = 0; i < numberofelements; i++)
+            {
+                result.Add(rand.Next());
+            }
+            return result;
+        }
+
+        public static List<int> CreateRandomArrayWithUpperLimit(int numberofelements, int maxnumber)
+        {
+            Random rand = new Random();
+
+            List<int> result = new List<int>();
+            for (int i = 0; i < numberofelements; i++)
+            {
+                result.Add(rand.Next(maxnumber));
+            }
+            return result;
+        }
+    }
+}
